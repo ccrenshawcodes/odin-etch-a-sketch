0cd5ff6910b593ea47c1b559897b4c95f5a102a4
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-02-13 12:27:02 -0700

    Adjust logic to make grid size-variable

--- app.js
+++ app.js
@@ -3,15 +3,40 @@ const container = document.querySelector('#container');
 
 
 function makeGrid(num) {
-    for (let i = 0; i < num; i++) {
+    let size = num * num;
+    for (let i = 0; i < size; i++) {
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
-        square.setAttribute('id', `sq${i}`);
-        square.style.cssText = 'background-color: blue'; 
-        square.textContent = `hello world ${i}`;
+        square.setAttribute('id', `sq${i}`); 
 
         container.appendChild(square);
     }
+    container.style.cssText = `grid-template-rows: repeat(${num}, 1fr)`;
+    container.style.cssText = `grid-template-columns: repeat(${num}, 1fr)`;
+    
 }
 
-makeGrid(16);
+makeGrid(50);
+
+/*function createGridOnClick() {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', () => {
+        let num = prompt('Enter a square number to create a grid to size')
+        makeGrid(Number(num));
+    })
+}
+
+createGridOnClick();*/
+
+/*function drawOnHover() {
+    const divs = document.querySelectorAll('.square');
+
+    divs.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            item.style.color = 'black';
+            console.log(item);
+        });
+    });
+}
+
+drawOnHover();*/
